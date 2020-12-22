d2a08d3f22e56428a227c0ee436f7a5d4b6884a8
Khalid Alhazmi <kalid@pop-os.localdomain>
2020-12-22 05:23:28 +0300

    done

--- app.py
+++ app.py
@@ -1,29 +1,32 @@
 from flask import Flask, render_template, request
 from send_mail import send_mail
 import datetime
-app = Flask(__name__)
+#app = Flask(__name__)
 
 
+def create_app(test_config=None):
+
+    app = Flask(__name__)
+
+    @app.route("/")
+    def send_email():
+        return render_template('mail.html')
 
 
+    @app.route('/result',methods = ['POST', 'GET'])
+    def result():
+        if request.method == 'POST':
+            result = request.form
+            receiver_mail = result['mail']
+            subject = result['subject']
+            message = result['message']
+            
+            send_mail(receiver_mail, subject , message)
+            return render_template("send.html")
 
-@app.route("/")
-def template_test():
-    return render_template('mail.html')
+    return app
 
-@app.route('/result',methods = ['POST', 'GET'])
-def result():
-   if request.method == 'POST':
-      result = request.form
-      receiver_mail = result['mail']
-      subject = result['subject']
-      message = result['message']
-    
-      send_mail(receiver_mail, subject , message)
-      return render_template("send.html")
-
-
-
+app = create_app()
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run()
